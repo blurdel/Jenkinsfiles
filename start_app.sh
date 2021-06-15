30b9be88df18825aabc4436a67325d5a91ba2ed0
blurdel <blurdel@gmail.com>
2021-06-14 20:09:17 -0400

    misc updates

---
+++ start_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+LOG="application.log"
+rm -f ${LOG}
+
+me=$(basename $0)
+echo "me=${me} $@"
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <file=filename.csv>"
+  exit
+fi
+
+function countDown {
+  COUNT=${1:-10}
+#  echo "$COUNT"
+  while (( COUNT > 0 )) ; do
+    echo -e "$COUNT \c"
+    (( COUNT-- ))
+    sleep 1
+  done
+  echo
+}
+
+
+echo "Doing Stuff..." >> ${LOG}
+countDown 6
+
+echo "Done."
