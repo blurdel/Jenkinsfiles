105aca482d20246ecae403ab5908c5f695d71ad1
blurdel <blurdel@gmail.com>
2021-07-15 14:02:50 -0400

    Update funcs.groovy

--- funcs.groovy
+++ funcs.groovy
@@ -1,4 +1,17 @@
 def echoBuild() {
   echo 'gs: building the application ...'
 }
+
+def echoTest() {
+  echo 'gs: testing the application ...'
+}
+
+def echoDeploy() {
+  echo 'gs: deploying the application ...'
+}
+
+def echoClean() {
+  echo 'gs: cleaning the application ...'
+}
+
 return this
